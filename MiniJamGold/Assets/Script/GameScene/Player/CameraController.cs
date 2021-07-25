5e0a9dacb1ecb0e737bfdfd9a4b8326c715ffb4d
Ben Laycock <benlaycock17@gmail.com>
2021-07-25 14:45:21 +0100

    DragonCave

--- MiniJamGold/Assets/Script/GameScene/Player/CameraController.cs
+++ MiniJamGold/Assets/Script/GameScene/Player/CameraController.cs
@@ -2,129 +2,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum TargetFocusDirection
+[System.Serializable]
+public class CameraTargetPosition
 {
-    eFront,
-    eRight,
-    eBack,
-    eLeft,
-    eUp,
-    eDown
-}
-
-public static class TargetFocusDirections
-{
-    public static Vector3[] mDirections = new Vector3[6] {
-            Vector3.forward,
-            Vector3.right,
-            Vector3.back,
-            Vector3.left,
-            Vector3.up,
-            Vector3.down,
-        };
+    public GameObject mTargetView = null;
+    public GameObject mTargetPosition = null;
 }
 
 public class CameraController : MonoBehaviour
 {
+    private static CameraController sInstance;
 
-    [SerializeField] private GameObject mTarget = null;
-    [SerializeField] private float mTargetRange = 10f;
-    [SerializeField] private float mTargetYOffset = 5f;
-    [SerializeField] private TargetFocusDirection mFocusDirection = TargetFocusDirection.eFront;
+    public static CameraController Instance { get { return sInstance; } }
 
-    private Queue<Vector3> mTargetPositions = new Queue<Vector3>();
-    private Vector3 mTargetCameraPosition = Vector3.zero;
+
+    private void Awake()
+    {
+        if (sInstance != null && sInstance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            sInstance = this;
+        }
+    }
+
+
+    [SerializeField] private List<CameraTargetPosition> mTargetPositions = new List<CameraTargetPosition>();
 
     [SerializeField] private float mMovementSpeed = 1f;
     [SerializeField] private float mAccuracyThreshold = 1f;
 
+    private CameraTargetPosition mIdealTarget = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        CalculateNextTargetPosition(mTarget);
-        PopNextTargetPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (mTarget != null) FocusTarget();
+        ManageTarget();
 
-        if (Vector3.Distance(transform.position, mTargetCameraPosition) < mAccuracyThreshold)
+        if (Input.GetKeyDown(KeyCode.N)) FindTargetPosition();
+    }
+
+    void ManageTarget()
+    {
+        if (mIdealTarget == null) return;
+
+        if (Vector3.Distance(transform.position, mIdealTarget.mTargetPosition.transform.position) > mAccuracyThreshold)
         {
-            if (mTargetPositions.Count > 0)
-            {
-                PopNextTargetPosition();
-            }
-        }
-        else
-        {
-            Vector3 directionToTarget = mTargetCameraPosition - transform.position;
+            Vector3 directionToTarget = mIdealTarget.mTargetPosition.transform.position - transform.position;
             transform.position += directionToTarget.normalized * mMovementSpeed * Time.deltaTime;
         }
-        
-        if (Input.GetKeyDown(KeyCode.N))
+
+        transform.LookAt(mIdealTarget.mTargetView.transform);
+    }
+
+    public void ForceTargetChange(CameraTargetPosition argTarget)
+    {
+        mIdealTarget = argTarget;
+    }
+
+    public void FindTargetPosition()
+    {
+        float distanceToIdealTarget = float.MaxValue;
+        foreach(CameraTargetPosition targetPos in mTargetPositions)
         {
-            CalculateNextTargetPosition(mTarget);
+            float distanceToTarget = Vector3.Distance(targetPos.mTargetPosition.transform.position, transform.position);
+            if (distanceToTarget > distanceToIdealTarget) continue;
+
+            distanceToIdealTarget = distanceToTarget;
+            mIdealTarget = targetPos;
         }
-    }
-
-    void FocusTarget()
-    {
-        transform.LookAt(mTarget.transform, Vector3.up);
-    }
-
-    void PopNextTargetPosition()
-    {
-        if (mTargetPositions.Count <= 0) return;
-        mTargetCameraPosition = mTargetPositions.Dequeue();
-    }
-
-    public void CalculateNextTargetPosition(GameObject argTargetObject)
-    {
-        Vector3 nextTargetPosition = GetTargetCameraPosition(argTargetObject);
-
-        mTargetPositions.Enqueue(nextTargetPosition);
-    }
-
-    Vector3 GetTargetCameraPosition(GameObject argTargetObject)
-    {
-        Vector3 targetPosition = argTargetObject.transform.position;
-
-        switch (mFocusDirection)
-        {
-            case TargetFocusDirection.eFront:
-                targetPosition += argTargetObject.transform.forward * mTargetRange +
-                    argTargetObject.transform.up * mTargetYOffset;
-                break;
-
-            case TargetFocusDirection.eRight:
-                targetPosition += argTargetObject.transform.right * mTargetRange +
-                    argTargetObject.transform.up * mTargetYOffset;
-                break;
-
-            case TargetFocusDirection.eBack:
-                targetPosition += -argTargetObject.transform.forward * mTargetRange +
-                    argTargetObject.transform.up * mTargetYOffset;
-                break;
-
-            case TargetFocusDirection.eLeft:
-                targetPosition += -argTargetObject.transform.right * mTargetRange +
-                    argTargetObject.transform.up * mTargetYOffset;
-                break;
-
-            case TargetFocusDirection.eUp:
-                targetPosition += argTargetObject.transform.up * mTargetRange;
-                break;
-
-            case TargetFocusDirection.eDown:
-                targetPosition += -argTargetObject.transform.up * mTargetRange;
-                break;
-
-            default:
-                break;
-        }
-
-        return targetPosition;
     }
 }
